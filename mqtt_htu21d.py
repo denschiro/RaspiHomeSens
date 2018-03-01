195ac2194fd459314ed5836998e8b9af2ff09c49
Denschiro <bradtmoeller@googlemail.com>
2018-03-01 21:18:13 +0100

    only send by change

--- mqtt_htu21d.py
+++ mqtt_htu21d.py
@@ -26,7 +26,6 @@ from sensor.HTU21D import HTU21D
 
 
 
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
@@ -36,8 +35,10 @@ client.on_connect = on_connect
 client.connect("192.168.178.202", 1883, 60)
 
 client.loop_start()
-
+hOld = 0
+tOld = 0
 while True:
+    
     time.sleep(20)
     # I2C bus=1, Address=0x40
     htu = HTU21D(1, 0x40)
@@ -48,10 +49,25 @@ while True:
     t = htu.temperature()  # read temperature
     #print(t)               # namedtuple
     #print(t.F)             # Fahrenheit
+    if hOld == h :
+        continue
+    else:
+           #h, t = htu.all()  # read both at once
+        client.publish("home/cellarleft/humidity", "pitwo, " + str(round(h.RH,1) )  )
+        #client.publish("home/cellarleft/temperature", "pitwo, " + str(round(t.C ,1)) )
 
-    #h, t = htu.all()  # read both at once
-    client.publish("home/cellarleft/humidity", "pitwo, " + str(round(h.RH,2) )  )
-    client.publish("home/cellarleft/temperature", "pitwo, " + str(round(t.C ,2)) )
+    hOld = h
+    
+
+    if tOld == t :
+        continue
+    else:
+           #h, t = htu.all()  # read both at once
+        #client.publish("home/cellarleft/humidity", "pitwo, " + str(round(h.RH,1) )  )
+        client.publish("home/cellarleft/temperature", "pitwo, " + str(round(t.C ,1)) )
+
+    tOld = t
+
    # time.sleep(10)
-    client.publish("home/cellarleft/humidityview",  str(round(h.RH,2) ))
-    client.publish("home/cellarleft/temperatureview",  str(round(t.C ,2) ))
+    #client.publish("home/cellarleft/humidityview",  str(round(h.RH,2) ))
+   # client.publish("home/cellarleft/temperatureview",  str(round(t.C ,2) ))
